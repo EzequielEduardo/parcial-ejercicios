f69f713c9bdae846bef884ee15e88f629c55553f
MATIAS <65511002+MATUKE23@users.noreply.github.com>
2021-07-14 12:40:58 -0300

    Add files via upload

---
+++ EJ Videos MI VERSION OK/Video.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+#include <cstring>
+#include "Video.h"
+
+Video::Video()
+{
+    //ctor
+}
+
+int Video::getID() { return _id; }
+void Video::setID(int val) { _id = val; }
+char* Video::getTitulo() { return _titulo; }
+void Video::setTitulo(const char *val) { strcpy(_titulo, val); }
+bool Video::getContenidoNinios() { return _ninios; }
+void Video::setContenidoNinios(bool val) { _ninios = val; }
+Fecha Video::getFecha() { return _fecha; }
+void Video::setFecha(Fecha val) { _fecha = val; }
+int Video::getDuracion() { return _duracion; }
+void Video::setDuracion(int val) { _duracion = val; }
+int Video::getVisualizaciones() { return _visualizaciones; }
+void Video::setVisualizaciones(int val) { _visualizaciones = val; }
+
+void Video::mostrar(){
+    cout << _id << "," << _titulo << "," << _ninios << "," << _fecha.getDia();
+    cout << "/" << _fecha.getMes() << "/" << _fecha.getAnio() << ",";
+    cout << _duracion << "," << _visualizaciones << endl;
+}
+
+void Video::mostrar2(){
+    cout << _id << "," << _titulo << ","<< _visualizaciones << endl;
+}
+
+bool Video::leerDeDisco(int pos){
+    FILE *p;
+    p = fopen("videos.dat", "rb");
+    if (p == NULL){    return false;   }
+    bool ok;
+    fseek(p, sizeof(Video) * pos , 0);
+    ok = fread(this, sizeof(Video), 1, p);
+    fclose(p);
+    return ok;
+}
+
+bool Video::grabarEnDisco(){
+    FILE *p;
+    p = fopen("videos.dat", "ab");
+    if (p == NULL){
+        return false;
+    }
+    bool ok;
+    ok = fwrite(this, sizeof(Video), 1, p);
+    fclose(p);
+    return ok;
+}
---
+++ EJ Videos MI VERSION OK/main.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+using namespace std;
+#include "Video.h"
+
+//-------------------------------------------------------------------------------------------
+//prototypes
+void Punto1();
+void ordenarClase(Video *vec, int tam);
+int contarRegVideos();
+void leeryllenarVectordeObjetos(Video *vec, int tam);
+void Ranking();
+void MostrarPorcentaje();
+
+//-------------------------------------------------------------------------------------------
+
+int calculaPorcentajeVideosdeAdulto(int anio){
+Video obj;
+int pos=0, cont=0;
+while(obj.leerDeDisco(pos++)){
+	if(obj.getFecha().getAnio()==anio && obj.getContenidoNinios()==false){
+	cont++;
+	}
+}
+return cont;
+//return (cont/contarRegVideos())*100;
+}
+
+
+int calculaPorcentajeVideosdeNinio(int anio){
+Video obj;
+int pos=0, cont=0;
+while(obj.leerDeDisco(pos++)){
+	if(obj.getFecha().getAnio()==anio && obj.getContenidoNinios()==true){
+	cont++;
+	}
+}
+cout<<"cant de vides de ninio: "<<cont;
+return cont;
+//return (cont/contarRegVideos())*100;
+}
+
+void MostrarPorcentaje(){
+int anioIni, anioFin;
+Video obj;
+cout<<"Ingrese año de inicio: "<<endl;
+cin>>anioIni;
+cout<<"ingrese año de fin: "<<endl;
+cin>>anioFin;
+
+
+for(int i=0;i<((anioFin-anioIni))+1;i++)
+{
+	cout<<"año: "<<anioIni+i<<"\t";
+	cout<<"% VideodeNinios: "<<calculaPorcentajeVideosdeNinio(anioIni+i)<<"\t";
+	cout<<"% Video para adultos: "<<calculaPorcentajeVideosdeAdulto(anioIni+i)<<endl;
+}
+
+
+
+
+}
+
+int main()
+{
+   // Punto1();
+  // Ranking();
+
+  MostrarPorcentaje();
+
+
+
+  //  reg.leerDeDisco(0);
+    //reg.mostrar();
+
+
+
+
+    return 0;
+}
+
+
+//-------------------------------------------------------------------------------------------
+//funciones globales
+void Punto1(){
+Video obj;
+int ID;
+cout<<"ingrese el Id: "<<endl;
+cin>>ID;
+
+int pos=0;
+
+while(obj.leerDeDisco(pos++)){
+	if(ID==obj.getID())	{
+		//cout<<"id: "<<obj.getID();
+		cout<<"TITULO: "<<obj.getTitulo()<<endl;
+		cout<<"DURACION: "<<obj.getDuracion()<<endl;
+		cout<<"VISUALIZACIONES: "<<obj.getVisualizaciones()<<endl;
+		cout<<"FECHA DE CREACION: ";obj.getFecha().mostrar();
+
+		obj.setVisualizaciones(obj.getVisualizaciones());
+		}
+	}
+}
+
+//-------------------------------------------------------------------------------------------
+
+void ordenarClase(Video *vec, int tam){
+    int i, j, posMax;
+    for(i=0; i<tam-1; i++){
+        posMax = i;
+        for(j=i+1; j<tam; j++){
+            if (vec[j].getVisualizaciones() > vec[posMax].getVisualizaciones()){
+                posMax = j;
+            }
+        }
+        Video aux = vec[i];
+        vec[i] = vec[posMax];
+        vec[posMax] = aux;
+    }
+}
+
+int contarRegVideos(){
+    int bytes, cr;
+    FILE *p;
+    p = fopen("videos.dat", "rb");
+    if (p == NULL){ return 0; }
+    fseek(p, 0, SEEK_END);
+    bytes = ftell(p);
+    cr = bytes / sizeof(Video);
+    fclose(p);
+    return cr;
+}
+
+void leeryllenarVectordeObjetos(Video *vec, int tam){
+    for(int i=0; i<tam; i++){
+        vec[i].leerDeDisco(i);
+    }
+}
+
+void Ranking(){
+int cant= contarRegVideos();
+Video *vec;
+if(cant ==0) return;
+vec=new Video[cant];
+leeryllenarVectordeObjetos(vec,cant);
+
+ordenarClase(vec,cant);
+
+for(int i=0;i<10;i++)
+{
+	vec[i].mostrar2();
+
+
+}
+
+
+delete vec;
+}
+
